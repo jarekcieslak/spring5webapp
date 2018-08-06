322dadc8b22dfa19894f1938f73e9d150a184033
Jaroslaw Cieslak <jarekcieslak@gmail.com>
2018-08-06 15:08:59 +0200

    Added publisher entity

--- src/main/java/guru/springframework/spring5webapp/model/Publisher.java
+++ src/main/java/guru/springframework/spring5webapp/model/Publisher.java
@@ -1,4 +1,38 @@
 package guru.springframework.spring5webapp.model;
 
+import org.hibernate.tuple.GeneratedValueGeneration;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Publisher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+
+    private String name;
+
+    public Publisher(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
